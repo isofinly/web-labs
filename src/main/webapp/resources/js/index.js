1b396911b0505acf298d6717011b3d6e6bf9d673
isofinly <isofinly@protonmail.com>
2023-11-04 22:09:03 +0300

    lab3 commit

---
+++ src/main/webapp/resources/js/index.js
@@ -0,0 +1,34 @@
+// clock
+const DELAY = 10000;
+
+function getDate(date) {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    day = (day < 10) ? '0' + day : day;
+    month = (month < 10) ? '0' + month : month;
+
+    return `${day}.${month}.${year}`;
+}
+
+function getTime(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    hours = (hours < 10) ? '0' + hours : hours;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+function setCurrentDateTime() {
+    const date = new Date();
+    const clock = document.querySelector('.main__datetime #my-clock');
+    clock.innerHTML = getDate(date) + ' ' + getTime(date)
+}
+
+setCurrentDateTime();
+setInterval(setCurrentDateTime, DELAY);
---
+++ src/main/webapp/resources/js/main.js
@@ -0,0 +1,57 @@
+let xValid = false, yValid = false, rValid = false;
+
+const messages = document.getElementById('messages');
+
+// X radio validation
+
+// let xInput = document.getElementById('form:X');
+// const xRadioInput = document.getElementById('xRadio');
+// const xRadioInputs = xRadioInput.querySelectorAll('input');
+// xRadioInputs.forEach(radio => {
+//     radio.addEventListener('input', () => {
+//         xInput.value = radio.value;
+//         xValid = true;
+//         toggleSubmitBtn();
+//     })
+// });
+
+let xInput = document.getElementById('form:X');
+const xSelect = document.getElementById('xSelect').querySelector('select');
+
+xSelect.addEventListener('change', () => {
+    xInput.value = xSelect.value;
+    xValid = true;
+    toggleSubmitBtn();
+});
+
+
+// Y input field validation
+let yInput = document.getElementById('form:Y');
+yInput.addEventListener('input', () => {
+    yValid = yInput.value.length > 0;
+    toggleSubmitBtn();
+})
+
+// R buttons validation
+let rInput = document.getElementById('form:R');
+const rRadioInput = document.getElementById('rRadio');
+const rRadioInputs = rRadioInput.querySelectorAll('input');
+rRadioInputs.forEach(radio => {
+    radio.addEventListener('input', () => {
+        rInput.value = radio.value;
+        rValid = true;
+        redrawGraph(rInput.value)
+        toggleSubmitBtn();
+    })
+});
+
+const submitBtn = document.getElementById('form:submitBtn');
+function toggleSubmitBtn() {
+    // check X, Y, R validity
+    submitBtn.disabled = !(xValid && yValid && rValid)
+}
+
+xInput.value = '';
+yInput.value = '';
+rInput.value = '';
+toggleSubmitBtn()
